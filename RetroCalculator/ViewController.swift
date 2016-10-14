c48760eb4445322ad2c2d34120898129ebb0d98c
TiciaChance <laticia.chance@flatironschool.com>
2016-10-14 13:40:12 -0400

    added math logic for calculator

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -9,9 +9,27 @@
 import UIKit
 import AVFoundation
 
+
 class ViewController: UIViewController {
     
+    @IBOutlet weak var calcLabel: UILabel!
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case empty = "Empty"
+    }
+    
+    var currentOperation = Operation.empty
+    var leftValueString = ""
+    var rightValueString = ""
+    var result = ""
+    
     var buttonSound = AVAudioPlayer()
+    
+    var runningNum = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,8 +52,38 @@ class ViewController: UIViewController {
         
         playSound()
         
+        runningNum += "\(sender.tag)"
+        
+        calcLabel.text = runningNum
+        
         
     }
+    
+    
+    @IBAction func divideButtonTapped(_ sender: AnyObject) {
+        processOperation(operation: .Divide)
+    }
+ 
+    @IBAction func multiplyButtonTapped(_ sender: AnyObject) {
+        processOperation(operation: .Multiply)
+
+    }
+    
+    @IBAction func subtractButtonTapped(_ sender: AnyObject) {
+        processOperation(operation: .Subtract)
+
+    }
+    
+    @IBAction func additionButtonTapped(_ sender: AnyObject) {
+        processOperation(operation: .Add)
+
+    }
+    
+    @IBAction func equalButtonTapped(_ sender: AnyObject) {
+        processOperation(operation: currentOperation)
+
+    }
+    
     
     func playSound() {
         
@@ -44,5 +92,45 @@ class ViewController: UIViewController {
         }
         
         buttonSound.play()
+    }
+    
+    //left hand value + operator + right hand value = result/running number
+    
+    func processOperation(operation: Operation) {
+        
+        if currentOperation != Operation.empty {
+            
+            // a check in case user selects an operator, but then selects another operator without first entering a # first
+            if !runningNum.isEmpty {
+                
+                rightValueString = runningNum
+                runningNum = ""
+                
+                if currentOperation == Operation.Multiply {
+                    result = "\(Double(leftValueString)) * \(Double(rightValueString))"
+                } else if currentOperation == Operation.Divide {
+                    result = "\(Double(leftValueString)) / \(Double(rightValueString))"
+                    
+                } else if currentOperation == Operation.Subtract {
+                    result = "\(Double(leftValueString)) - \(Double(rightValueString))"
+                } else if currentOperation == Operation.Add {
+                    result = "\(Double(leftValueString)) + \(Double(rightValueString))"
+                    
+                }
+                
+                leftValueString = result
+                calcLabel.text = result
+            }
+            currentOperation = operation
+        } else {
+           
+            // if it is the first time the operation has been pressed
+            leftValueString = runningNum
+            runningNum = ""
+            
+            currentOperation = operation
+        }
+        
+        
     }
 }
